9af20e6ebe6221f16d530587a24a16f5c026c9fd
MabeDiaby <diabymabe@gmail.com>
2022-11-30 21:01:50 -0500

    notes

--- notes/notes.py
+++ notes/notes.py
@@ -1,1 +1,29 @@
 # While loops
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Making our own function
+def my_function():
+    print("Hello")
+    print("Bye")
+# Calling function
+my_function()
+
+# Indentation
+sky = "clear"
+def my_functions():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+print("World")
+# Calling function
+my_functions()
+
+# While loops
+num_of_hurdles = 6
+while num_of_hurdles > 0:
+    print(num_of_hurdles)
+    num_of_hurdles -= 1
